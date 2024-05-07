eef2023cf68b82934ea39e1079e35c03dc205cea
alexxmec <154478521+alexxmec@users.noreply.github.com>
2024-05-07 20:29:38 -0300

    Add files via upload

---
+++ ptt1.py
@@ -0,0 +1,13 @@
+from playsound import playsound
+
+print("identifique-se")
+usu = input("user: ")
+pas = input("password: ")
+
+if pas == ("menor123"):
+    cam = ("C:/Users/mcgam/Documents/eves/bomdia.mp3")
+    playsound(cam)
+    
+    input()
+
+input()
